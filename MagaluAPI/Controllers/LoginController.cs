015de7755a8e0c4fc515cc865e64fdbf4b66f830
unknown <jr_paulo1999@hotmail.com>
2023-11-11 02:21:16 -0300

    final commit

--- MagaluAPI/Controllers/LoginController.cs
+++ MagaluAPI/Controllers/LoginController.cs
@@ -1,5 +1,7 @@
 using MagaluAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Data.SqlClient;
 
 [Route("api/[controller]")]
@@ -10,7 +12,7 @@ public class LoginController : ControllerBase
 
     public LoginController()
     {
-        _connectionString = "Data Source=DESKTOP-I6L8BA5\\SQLEXPRESS;Initial Catalog=Magalu;Integrated Security=true;";
+        _connectionString = "Data Source=DESKTOP-I6L8BA5;Initial Catalog=Magalu;Integrated Security=True;";
     }
 
     [HttpPost]
@@ -21,12 +23,50 @@ public class LoginController : ControllerBase
             return BadRequest("Solicitação inválida");
         }
 
-        if (usuario.Username == "Magalu" && usuario.Password == "m@galu123")
+        if (ValidarCredenciais(usuario.Username, usuario.Password))
         {
+            AdicionarCookieAutenticacao(usuario.Username);
+
             return Ok();
         }
 
         return Unauthorized("Credenciais inválidas");
     }
 
+    private bool ValidarCredenciais(string username, string password)
+    {
+        try
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT COUNT(*) FROM Usuario WHERE Username = @Username AND Password = @Password";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Username", username);
+                    command.Parameters.AddWithValue("@Password", password);
+
+                    int result = (int)command.ExecuteScalar();
+
+                    return result > 0;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erro ao validar credenciais: {ex.Message}");
+            return false;
+        }
+    }
+
+    private void AdicionarCookieAutenticacao(string username)
+    {
+        var option = new CookieOptions
+        {
+            Expires = DateTime.Now.AddDays(1)
+        };
+        Response.Cookies.Append("UsuarioAutenticado", username, option);
+    }
 }
